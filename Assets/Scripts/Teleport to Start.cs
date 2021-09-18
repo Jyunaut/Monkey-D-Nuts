7f786e724183539f9ce7bd9b3d10a69b7ea5b9c9
Benedict Wong <bwa44@sfu.ca>
2021-09-17 22:51:21 -0700

    Update Teleport to Start.cs

--- Assets/Scripts/Teleport to Start.cs
+++ Assets/Scripts/Teleport to Start.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TeleporttoStart : MonoBehaviour {
-	public GameObject portalOne, player;
-	
+	public GameObject portalOther, player;
+	public String horizontalOrVertical;
+	private float lastPlayerX, lastPlayerY;
+	private Vector3 playerPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +21,56 @@ public class TeleporttoStart : MonoBehaviour {
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
+	    var portalRendererBounds = portalOther.GetComponent<Renderer>();
+	    float portalWidth = portalRendererBounds.bounds.size.x;
+	    float portalHeight = portalRendererBounds.bounds.size.y;
+	    var playerRendererBounds = player.GetComponent<Renderer>();
+	    float playerWidth = playerRendererBounds.bounds.size.x;
+	    float playerHeight = playerRendererBounds.bounds.size.y;
+	    
 	    if (other.gameObject.CompareTag("Player")) {
-		    var rendererBounds = portalOne.GetComponent<Renderer>();
-		    float width = rendererBounds.bounds.size.x;
-		    Vector3 playerPos;
-		    playerPos = new Vector3(portalOne.transform.position.x+width/2, player.transform.position.y, player.transform.position.z);
-		    player.transform.position = playerPos;
+		    if (horizontalOrVertical == "horizontal") {
+			    lastPlayerX = player.transform.position.x;
+			    
+			    // If player hits top boundary
+			    if (player.transform.position.y + playerHeight / 2 <=
+				    portalOther.transform.position.y - portalHeight / 2) {
+				    playerPos = new Vector3(lastPlayerX, portalOther.transform.position.y + portalHeight/2 + playerHeight/2, player.transform.position.z);
+				    player.transform.position = playerPos;
+			    }
+			    
+			    // If player hits bottom boundary
+			    else if (player.transform.position.y - playerHeight / 2 >=
+				    portalOther.transform.position.y + portalHeight / 2) {
+				    playerPos = new Vector3(lastPlayerX, portalOther.transform.position.y - portalHeight/2 - playerHeight/2, player.transform.position.z);
+				    player.transform.position = playerPos;
+			    }
+			    
+			    
+		    }
+		    else if (horizontalOrVertical == "vertical") {
+			    lastPlayerY = player.transform.position.y;
+
+			    if (player.transform.position.x + playerWidth / 2 <=
+				    portalOther.transform.position.x - portalWidth / 2) {
+				    playerPos = new Vector3(portalOther.transform.position.x + portalWidth/2 + playerWidth/2, lastPlayerY, player.transform.position.z);
+				    player.transform.position = playerPos;
+				    
+			    }
+			    else if (player.transform.position.x - playerWidth / 2 <=
+				    portalOther.transform.position.x + portalWidth / 2) {
+				    playerPos = new Vector3(portalOther.transform.position.x - portalWidth/2 - playerWidth/2, lastPlayerY, player.transform.position.z);
+				    player.transform.position = playerPos;
+				    
+			    }
+			    
+		    }
+
+		    
+		    
+		    
+		    
+		    
 
 	    }
     }
