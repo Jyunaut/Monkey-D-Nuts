d8b5b5b76adc0df71bef3187d8168cf44fd8c9a3
CRAKAAW <crowshiding@gmail.com>
2021-09-18 21:48:43 -0700

    Fixed level transition

--- Assets/Scripts/Interactibles/LevelTransitioner.cs
+++ Assets/Scripts/Interactibles/LevelTransitioner.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class LevelTransitioner : Interactible
 {
-    public override void DoBehaviour()
-    {
-        base.DoBehaviour();
-        GameEvent.CompleteLevel();
-        EffectsManager.Instance.ScreenShake(0f, 3f, 90f, 5f, 8f);
-        Stop();
-    }
+    private Action _doTransitionLevel;
 
     protected override void Awake()
     {
         base.Awake();
         DontDestroyOnLoad(gameObject);
+        _doTransitionLevel = TransitionLevel;
+    }
+
+    public override void DoBehaviour()
+    {
+        base.DoBehaviour();
+        if(_doTransitionLevel != null)
+            _doTransitionLevel();
+    }
+
+    private void TransitionLevel()
+    {
+        GameEvent.CompleteLevel();
+        EffectsManager.Instance.ScreenShake(0f, 3f, 90f, 5f, 8f);
+        _doTransitionLevel = null;
+        Stop();
     }
 }
