fa1879f7b9c79a3aaea4a0afa7975caa346a5ce2
Benedict Wong <bwa44@sfu.ca>
2021-09-19 12:23:40 -0700

    title screen changes

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour {
+	public Button exitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitButton.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick() {
+	  Application.Quit();  
+    }
+}
