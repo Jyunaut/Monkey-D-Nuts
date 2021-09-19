b611430ad6b726e678dbe26256806e1236468e79
CRAKAAW <crowshiding@gmail.com>
2021-09-18 21:25:22 -0700

    Updated Level transitioner to be interactible

--- Assets/Scripts/Interactibles/LevelTransitioner.cs
+++ Assets/Scripts/Interactibles/LevelTransitioner.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelTransitioner : ButtonReceiver
+public class LevelTransitioner : Interactible
 {
-    public override void Trigger()
+    public override void DoBehaviour()
     {
+        base.DoBehaviour();
         GameEvent.CompleteLevel();
         EffectsManager.Instance.ScreenShake(0f, 3f, 90f, 5f, 8f);
+        Stop();
     }
 
-    public void Awake()
+    protected override void Awake()
     {
+        base.Awake();
         DontDestroyOnLoad(gameObject);
     }
 }
