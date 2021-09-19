c5d5821da6b9c0e80a55b08267bedb6d82202f1e
Jyunaut <edmartinh@gmail.com>
2021-09-18 19:10:09 -0700

    Added Effects Manager

--- Assets/Scripts/Interactibles/LevelTransitioner.cs
+++ Assets/Scripts/Interactibles/LevelTransitioner.cs
@@ -7,6 +7,7 @@ public class LevelTransitioner : ButtonReceiver
     public override void Trigger()
     {
         GameEvent.CompleteLevel();
+        EffectsManager.Instance.ScreenShake(0f, 3f, 90f, 5f, 8f);
     }
 
     public void Awake()
